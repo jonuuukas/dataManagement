1af75a31e4d255542696ad693f364d102fe1618e
Jonas Sideravičius <jonas.sideravicius@gmail.com>
2016-07-05 17:08:10 +0200

    added tooltips module and tooltips on the main buttons

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -688,3 +688,19 @@ myApp.directive("inlineEditable", function () {
         }
     }
 });
+
+//======================Tooltip directive=======================//
+myApp.directive('tooltip', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            $(element).hover(function(){
+                // on mouseenter
+                $(element).tooltip('show');
+            }, function(){
+                // on mouseleave
+                $(element).tooltip('hide');
+            });
+        }
+    };
+});
