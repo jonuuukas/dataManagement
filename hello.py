efbcd857c49f1398df3e40f90a939cb14d1f3147
Jonas Sideravičius <jonas.sideravicius@gmail.com>
2016-09-06 12:45:07 +0200

    adding local config file for jsiderav user, config parser and modified hello.py script to use the CONFIG values

--- hello.py
+++ hello.py
@@ -9,16 +9,19 @@ import sys
 import datetime
 import xml.dom.minidom
 import imp
+
 from couchdb_interface import CouchDBInterface
+from config import CONFIG
+
 
 from flask import Flask, send_from_directory, redirect, Response, make_response, request, jsonify
 from subprocess import Popen, PIPE
 app = Flask(__name__)
 ###original
-WORK_DIR = '/afs/cern.ch/user/j/jsiderav/public/dataManagement/stuff'
+WORK_DIR = CONFIG.WORK_DIR
 WORKDIR = ''
 couch = CouchDBInterface()
-cred = '/afs/cern.ch/user/j/jsiderav/private/PdmVService.txt'
+cred = CONFIG.PRIV_CRED
 
 ###
 ###Replaced with below
@@ -30,6 +33,7 @@ def hello():
     """
     Opening function that directs to index.html
     """
+    print CONFIG.USER_PROXY
     return send_from_directory('templates', 'index.html')
 @app.route('/api/help', methods = ['GET'])
 def help():
@@ -45,7 +49,7 @@ def get_scram(__release):
     and returns the architecture name if found
     """
     scram = ''
-    xml_data = xml.dom.minidom.parseString(os.popen("curl -s --insecure 'https://cmssdt.cern.ch/SDT/cgi-bin/ReleasesXML/?anytype=1'").read())
+    xml_data = xml.dom.minidom.parseString(os.popen("curl -s --insecure '%s'" % CONFIG.REL_XML).read())
     
     for arch in xml_data.documentElement.getElementsByTagName("architecture"):
         scram_arch = arch.getAttribute('name')
@@ -132,7 +136,7 @@ def get_skim_matrix_val():
     """
     file = "temporarySkim.py"
     data = json.loads(request.get_data())
-    url = "https://raw.githubusercontent.com/cms-sw/cmssw/"+data['CMSSW']+"/Configuration/Skimming/python/autoSkim.py"
+    url = CONFIG.GIT_CMSSW + data['CMSSW'] + "/Configuration/Skimming/python/autoSkim.py"
     response = urllib2.urlopen(url)
     fh = open(file, 'w')
     fh.write(response.read())
@@ -148,7 +152,7 @@ def get_alca_matrix_val():
     """
     file = "temporaryAlca.py"
     data = json.loads(request.get_data())
-    url = "https://raw.githubusercontent.com/cms-sw/cmssw/"+data['CMSSW']+"/Configuration/AlCa/python/autoAlca.py"
+    url = CONFIG.GIT_CMSSW+data['CMSSW']+"/Configuration/AlCa/python/autoAlca.py"
     response = urllib2.urlopen(url)
     fh = open(file, 'w')
     fh.write(response.read())
@@ -181,8 +185,8 @@ def get_test_bash(__release, _id, __scram):
     comm += "cd %s/src\n" % (__release)
     comm += "rm step_make.py\n"
     comm += "rm couchdb_interface.py\n"
-    comm += "wget https://raw.githubusercontent.com/jonuuukas/dataManagement/master/step_make.py\n"
-    comm += "wget https://raw.githubusercontent.com/jonuuukas/dataManagement/master/couchdb_interface.py\n"
+    comm += "wget %s\n" %CONFIG.GIT_STEP_MAKE
+    comm += "wget %s\n" %CONFIG.GIT_COUCHDB
     # comm += "eval `scram runtime -sh`\n"
     comm += "eval `scram runtime -sh` && python step_make.py --in=%s\n" % (_id)
     comm += "cat %s | voms-proxy-init -voms cms -pwstdin\n" %(cred)
@@ -210,18 +214,18 @@ def get_submit_bash(__release, _id, __scram):
     # comm += "cd Configuration/Skimming\n"
     # comm += "wget https://raw.githubusercontent.com/cms-sw/cmssw/CMSSW_8_0_1/Configuration/Skimming/python/autoSkim.py\n"
     #####################LOCAL REPO IS CURRENTLY BEING USED FOR THE WGET LINES#################
-    comm += "wget https://raw.githubusercontent.com/jonuuukas/dataManagement/master/step_make.py\n"
-    comm += "wget https://raw.githubusercontent.com/jonuuukas/dataManagement/master/couchdb_interface.py\n"
+    comm += "wget %s\n" %CONFIG.GIT_STEP_MAKE
+    comm += "wget %s\n" %CONFIG.GIT_COUCHDB
     ######################################SEE ABOVE, NOOB######################################
     comm += "python step_make.py --in=%s\n" % (_id)
     #-------------For wmcontrol.py---------------------
-    comm += "source /afs/cern.ch/cms/PPD/PdmV/tools/wmclient/current/etc/wmclient.sh\n" 
+    comm += "source %s\n" %CONFIG.WMCLIENT
     comm += "export PATH=/afs/cern.ch/cms/PPD/PdmV/tools/wmcontrol_testful:${PATH}\n"
     comm += "cat %s | voms-proxy-init -voms cms -pwstdin\n" %(cred)
     comm += "echo 'executing scram runtime'\n"
     comm += "eval `scram runtime -sh`\n"
     comm += "echo 'executing export'\n"
-    comm += "export X509_USER_PROXY=$(voms-proxy-info --path)\n"
+    comm += "export X509_USER_PROXY="+CONFIG.USER_PROXY+"\n"
     comm += "echo 'executing step wmcontrol.py'\n"
     comm += "wmcontrol.py --wmtest --req_file=master.conf\n"
     #--------------------------------------------------
@@ -238,7 +242,7 @@ def das_driver_all():
         Local scope method which loops through the result of das_client queries, 
         exits on first found result, doesn't bother too much
         """
-        proc = subprocess.call("eval `scramv1 runtime -sh`; cat /afs/cern.ch/user/j/jsiderav/private/PdmVService.txt | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY=$(voms-proxy-info --path); das_client.py  --limit 0 --query 'site dataset="+key+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log_file,stderr=err_file,shell=True)
+        proc = subprocess.call("eval `scramv1 runtime -sh`; cat "+cred+" | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY="+CONFIG.USER_PROXY+"; das_client.py  --limit 0 --query 'site dataset="+key+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log_file,stderr=err_file,shell=True)
         log_file.seek(0)
         err_file.seek(0)
         text = log_file.read()
@@ -247,7 +251,7 @@ def das_driver_all():
             for site in item['site']:
                 if 'dataset_fraction' in site and site['kind']=="Disk":
                     print site['name']
-                    proc = subprocess.call("eval `scramv1 runtime -sh`; cat /afs/cern.ch/user/j/jsiderav/private/PdmVService.txt | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY=$(voms-proxy-info --path); das_client.py  --limit 10 --query 'file dataset="+key+" site="+site['name']+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log2_file, stderr=err2_file, shell=True)
+                    proc = subprocess.call("eval `scramv1 runtime -sh`; cat "+cred+" | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY="+CONFIG.USER_PROXY+"; das_client.py  --limit 10 --query 'file dataset="+key+" site="+site['name']+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log2_file, stderr=err2_file, shell=True)
                     log2_file.seek(0)
                     err2_file.seek(0)
                     text2 = log2_file.read()
@@ -298,7 +302,7 @@ def das_driver_single():
     err_file = file('dasTest_errSingle.txt','w+')
     log2_file = file('dasTest_2outSingle.txt','w+')
     err2_file = file('dasTest_2errSingle.txt','w+')
-    proc = subprocess.call("eval `scramv1 runtime -sh`; cat /afs/cern.ch/user/j/jsiderav/private/PdmVService.txt | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY=$(voms-proxy-info --path); das_client.py  --limit 0 --query 'site dataset="+dsName+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log_file,stderr=err_file,shell=True)
+    proc = subprocess.call("eval `scramv1 runtime -sh`; cat "+cred+" | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY="+CONFIG.USER_PROXY+"; das_client.py  --limit 0 --query 'site dataset="+dsName+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log_file,stderr=err_file,shell=True)
     log_file.seek(0)
     err_file.seek(0)
     text = log_file.read()
@@ -307,7 +311,7 @@ def das_driver_single():
         for site in item['site']:
             if 'dataset_fraction' in site and site['kind']=="Disk":
                 print site['name']
-                proc = subprocess.call("eval `scramv1 runtime -sh`; cat /afs/cern.ch/user/j/jsiderav/private/PdmVService.txt | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY=$(voms-proxy-info --path); das_client.py  --limit 10 --query 'file dataset="+dsName+" site="+site['name']+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log2_file, stderr=err2_file, shell=True)
+                proc = subprocess.call("eval `scramv1 runtime -sh`; cat "+cred+" | voms-proxy-init -voms cms -pwstdin > dasTest_voms.txt;export X509_USER_PROXY="+CONFIG.USER_PROXY+"; das_client.py  --limit 10 --query 'file dataset="+dsName+" site="+site['name']+"' --key=$X509_USER_PROXY --cert=$X509_USER_PROXY --format=json",stdout=log2_file, stderr=err2_file, shell=True)
                 log2_file.seek(0)
                 err2_file.seek(0)
                 text2 = log2_file.read()
@@ -339,6 +343,7 @@ def test_campaign():
     req = data['req']
     doc = json.dumps(data['doc'])
     __scram = get_scram(__release)
+    print (__scram)
     if __scram == '':
         return "No scram"
     #----------Creating & running bash file----------------------
